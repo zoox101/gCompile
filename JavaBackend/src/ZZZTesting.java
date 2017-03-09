b613befc4352d4eabf35708f4d0d526aae8c91de
zoox101 <william@thebookers.net>
2017-03-08 22:30:25 -0600

    Added Jar File

--- JavaBackend/src/ZZZTesting.java
+++ JavaBackend/src/ZZZTesting.java
@@ -1,29 +1,34 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document; 
 
 public class ZZZTesting {
 	
 	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
 		
+		/*
 		String fileId = "0BwwA4oUTeiV1UVNwOHItT0xfa2M";
 		OutputStream outputStream = new ByteArrayOutputStream();
 		driveService.files().get(fileId)
 		        .executeMediaAndDownloadTo(outputStream);
+		        */
 
 		
 		//System.out.println(Bash.run("echo testing12345")[1]);
-		//String[] io = Bash.run("curl https://docs.google.com/document/d/1ULCRSEaBhw7oJ4Way2GQU0dDM62IiQUmO5LARWWZydQ/edit?usp=sharing");
-		//String[] io = Bash.run("curl https://docs.google.com/document/d/1ULCRSEaBhw7oJ4Way2GQU0dDM62IiQUmO5LARWWZydQ/edit?usp=sharing");
+		
+		String input = Bash.run("curl https://docs.google.com/document/d/1ULCRSEaBhw7oJ4Way2GQU0dDM62IiQUmO5LARWWZydQ/edit?usp=sharing")[0];
+		Document soup = Jsoup.parse(input);
+		System.out.println(soup.text());
+		
+		
+		//String[] io = Bash.run("curl https://www.google.com");
 		//Bash.run("javac Test.java");
 		//String[] io = Bash.run("java Test");
 		//Bash.run("jar cfm Test.jar Manifest.txt *.class");
 		//String[] io = Bash.run("java -cp Test.jar");
+		
 		
 		//System.out.println(io[0]);
 
