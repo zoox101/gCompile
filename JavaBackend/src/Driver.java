fb3a8f9777b0024fe7b8275218dbe2eb7e342b41
Catherine Ha <catherine.an.ha@gmail.com>
2017-04-06 00:40:42 -0500

    Added dynamic file add functionality

--- JavaBackend/src/Driver.java
+++ JavaBackend/src/Driver.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -33,7 +34,12 @@ import javafx.stage.Stage;
 
 
 public class Driver extends Application{
-
+	Button plusButton;
+	GridPane grid;
+	AtomicInteger numAddedFiles;
+	AtomicInteger numPresetFilesToAdd;
+	
+	
 	public static void main(String args[]) throws IOException {
 
 		System.out.println("Running");
@@ -69,41 +75,51 @@ public class Driver extends Application{
 		
 		Group root = new Group(); //create the root
 		ScrollPane sp = new ScrollPane();
-		GridPane grid = new GridPane();
+		grid = new GridPane(); 
 		
-		Button plusButton = new Button("+");
+		
+		plusButton = new Button("+");
+		numAddedFiles = new AtomicInteger(); //initializes to 0
+		numPresetFilesToAdd = new AtomicInteger();
+		plusButton.setOnAction(e -> pressButton(e));
 		plusButton.setMaxHeight(10);
 		plusButton.setMaxWidth(10);
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(2);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
+		
 		//sp.fitToHeightProperty();
+		
 		sp.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		sp.setHbarPolicy(ScrollBarPolicy.NEVER);
 		sp.setContent(grid);
 		Scene scene = new Scene(sp, 370, 275);
 		
+		Button compileBtn = new Button("Compile");
+		compileBtn.setMaxWidth(200);;
+		grid.add(compileBtn, 1, 0, 2, 1);
+		//Text scenetitle = new Text("Select Documents to compile.");
+		//scenetitle.setFont(Font.font("", FontWeight.NORMAL, 14));
+		//grid.add(scenetitle, 0, 0, 2, 1);
 		
-		Text scenetitle = new Text("Select Documents to compile.");
-		scenetitle.setFont(Font.font("", FontWeight.NORMAL, 14));
-		grid.add(scenetitle, 0, 0, 2, 1);
-		grid.add(plusButton, 3,2);
 		Label doc1Label = new Label("Google Doc 1:");
-		grid.add(doc1Label, 0, 1);
+		grid.add(doc1Label, 0, numPresetFilesToAdd.incrementAndGet());
 
 		TextField doc1Text = new TextField();
-		grid.add(doc1Text, 1, 1);
-
+		grid.add(doc1Text, 1, numPresetFilesToAdd.get());
+		
 		Label doc2Label = new Label("Google Doc 2:");
-		grid.add(doc2Label, 0, 2);
-
+		grid.add(doc2Label, 0, numPresetFilesToAdd.incrementAndGet());
+		grid.add(plusButton, 3, numPresetFilesToAdd.get());
+		
 		TextField doc2Text = new TextField();
-		grid.add(doc2Text, 1, 2);
+		grid.add(doc2Text, 1, numPresetFilesToAdd.get());
 		ScrollBar s = new ScrollBar();
 		s.setOrientation(Orientation.VERTICAL);
 		
-		Button compileBtn = new Button("Compile");
+		
+/*		Button compileBtn = new Button("Compile");
 		HBox hbBtn = new HBox(5);
 		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 		hbBtn.getChildren().add(compileBtn);
@@ -117,81 +133,36 @@ public class Driver extends Application{
                 actiontarget.setFill(Color.FIREBRICK);
                 actiontarget.setText("Compile button pressed");
             }
-        });
-//		primaryStage.setScene(scene);
-//		 Button btn = new Button();
-//	        btn.setText("Compile");
-//	        
-//	        
-//	        
-//	        btn.setOnAction(new EventHandler<ActionEvent>() {
-//	 
-//	            @Override
-//	            public void handle(ActionEvent event) {
-//	                System.out.println("Hello1 World!");
-//	            }
-//	        });
-//	        
-//	      
-//	        root.getChildren().add(btn);
-//	       btn.setLayoutX(170);
-//	       btn.setLayoutY(140);
-//	       
-//
-//	 Scene scene = new Scene(root, 400, 200);
-//
-//	        primaryStage.setTitle("HaWillJavaCompile");
-//	        primaryStage.setScene(scene);
-//	        
-//	        Label label1 = new Label("GoogleDoc URL:");
-//	        label1.setLayoutX(60);
-//	        label1.setLayoutY(100);
-//	        TextField textField = new TextField ("Ello");
-//	      
-//	      //  HBox hb = new HBox();
-//	        root.getChildren().addAll(label1, textField);
-//	     //   hb.setSpacing(10);
-//	        textField.setLayoutX(180);
-//	        textField.setLayoutY(100);
-	      
-	    /*    
-	        GridPane grid = new GridPane();
-	        grid.setAlignment(Pos.CENTER);
-	        grid.setHgap(10);
-	        grid.setVgap(10);
-	        grid.setPadding(new Insets(25, 25, 25, 25));
-
-	        Text scenetitle = new Text("Welcome");
-	        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-	        grid.add(scenetitle, 0, 0, 2, 1);
-
-	        Label userName = new Label("User Name:");
-	        grid.add(userName, 0, 1);
-
-	        TextField userTextField = new TextField();
-	        grid.add(userTextField, 1, 1);
-
-	        Label pw = new Label("Password:");
-	        grid.add(pw, 0, 2);
-
-	        PasswordField pwBox = new PasswordField();
-	        
-	        
-	        Button btn1 = new Button("Sign in");
-	        HBox hbBtn = new HBox(10);
-	        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-	        hbBtn.getChildren().add(btn1);
-	        grid.add(hbBtn, 1, 4);
-	        
-	      //  final Text actiontarget = new Text();
-	       // grid.add(actiontarget, 1, 6);
-	        
-	   
-	        grid.add(pwBox, 1, 2);*/
+        });*/
+    
 	       
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
 	}
+    
+    public void pressButton(ActionEvent e){
+    	if(e.getSource().toString().contains("+")){
+    		Button tempAddBtn = new Button("+");
+    		tempAddBtn.setOnAction(e2 -> pressDynamicButton (e2));
+    		grid.add(tempAddBtn, 3, numPresetFilesToAdd.get() + numAddedFiles.incrementAndGet());
+    		grid.add(new Label("Google Doc " + (numPresetFilesToAdd.get() + numAddedFiles.get()) + ":"),
+    				0, numPresetFilesToAdd.get()+ numAddedFiles.get());
+    		grid.add(new TextField(), 1, numPresetFilesToAdd.get() + numAddedFiles.get());
+
+    	}
+    
+    }
+    public void pressDynamicButton(ActionEvent e2){
+    	if(e2.getSource().toString().contains("+")){
+    		Button tempAddBtn = new Button("+");
+    		tempAddBtn.setOnAction(e -> pressButton(e));
+    		grid.add(tempAddBtn, 3, numPresetFilesToAdd.get() + numAddedFiles.incrementAndGet());
+    		grid.add(new Label("Google Doc " + (2 + numAddedFiles.get()) + ":"), 
+    				0, numPresetFilesToAdd.get() + numAddedFiles.get());
+    		grid.add(new TextField(), 1, numPresetFilesToAdd.get() + numAddedFiles.get());
+    	}
+    }
+    
 
 }
 
