d2f4c9cb4c9bb87477bb72aad9bd0d4f106f3ca1
Catherine Ha <catherine.an.ha@gmail.com>
2017-04-26 19:38:08 -0500

    Finally got around to comment and put the project in MVC -.-

---
+++ JavaBackend/src/FrontEnd/View.java
@@ -0,0 +1,153 @@
+package FrontEnd;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import MiddleEarth.Controller;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import model.Model;
+
+/**
+ * This program allows the user to input google doc urls so that they can compile
+ * code from google docs locally and get a console output without having to copy and 
+ * paste code back and forth from google doc to their local machine.
+ */
+public class View extends Application{
+	
+	// Set size of the Frame
+	private final int APPWIDTH = 400;
+	private final int APPHEIGHT = 500;
+	
+	// Initialize Model and Controller
+	private Model m = new Model();
+	private Controller c = new Controller();
+	
+	// Items for GUI
+	private Button plusButton;
+	private Accordion accordion;
+	private TextArea outputText;
+
+	// Main Method
+	public static void main(String args[]){
+		launch(args);
+	}
+
+	/**
+	 *  This is for the UI so that the user can add google documents and compile 
+	 *  them to get a console output.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		// Create the main pane that the user will interact with
+		FlowPane outputPane = new FlowPane();
+		
+		// Set the output Scene with the pane and Frame dimensions
+		Scene outputScene = new Scene(outputPane, APPWIDTH, APPHEIGHT);
+	
+		/******************** Add documents to compile ************************/	
+		
+		// Allow user to scroll when user has an un-godly number of files to compile
+		ScrollPane sp = new ScrollPane();
+		
+		// Create accordion where user can modify files to compile
+		TitledPane tps = new TitledPane("Add Google Documents", sp);
+	    accordion = new Accordion ();  
+	    accordion.getPanes().addAll(tps);
+		accordion.setMinWidth(APPWIDTH);
+		accordion.setMaxHeight(APPHEIGHT);
+		outputPane.getChildren().add(accordion);
+	    
+		// Add the plus button that allows user to add more files
+		plusButton = new Button("+");
+		plusButton.setTooltip(new Tooltip("Add more Google documents to compile"));
+		plusButton.setMaxHeight(10);
+		plusButton.setMaxWidth(10);
+		plusButton.setOnAction( e ->c.pressButton(e, m));
+		
+		// Create compile button which prompts user to select a directory if 
+		// one has not yet been set
+		Button compileBtn = new Button("Compile");
+		compileBtn.setTooltip(new Tooltip("Select a directory to save to"));
+		compileBtn.setOnAction(e -> c.pressButton(e, m));
+		compileBtn.setMaxWidth(200);
+				
+		// Create grid for compile button, plus button, labels, and text Area
+		GridPane grid = new GridPane(); 
+		grid.setAlignment(Pos.TOP_CENTER);
+		grid.setHgap(2);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		grid.setPrefHeight(APPHEIGHT);
+		grid.setPrefWidth(APPWIDTH);
+		grid.add(compileBtn, 1, 0, 2, 1);
+		
+		// Increment the numbers of file labels that are being added to the GUI
+		m.setNumPresetFilesToAdd(m.getNumPresetFilesToAdd() + 1);
+		
+		// Add input doc label
+		Label doc1Label = new Label("Google Doc 1:");
+		grid.add(doc1Label, 0, m.getNumPresetFilesToAdd());
+
+		// Add text field to grid
+		TextField doc1Text = new TextField();
+		grid.add(doc1Text, 1, m.getNumPresetFilesToAdd());
+		grid.add(plusButton, 3,m.getNumPresetFilesToAdd());
+		
+		// Update grid pane to model
+		m.setGridpane(grid);
+	
+		// Limit the scroll bar 
+		sp.setPrefHeight(APPHEIGHT-50);
+		sp.setHbarPolicy(ScrollBarPolicy.NEVER);
+		sp.setPrefWidth(accordion.getWidth());
+		sp.setContent(m.getGridpane());
+		
+		/***************** Create area for the output to print to ***************/	
+		Pane consolePane = new Pane();
+		outputText = new TextArea("Output");
+		outputText.setMinSize(APPWIDTH, APPHEIGHT-25);
+		outputText.setMaxSize(APPWIDTH, APPHEIGHT-25);
+		consolePane.getChildren().add(outputText);
+		consolePane.setMaxSize(APPWIDTH, APPHEIGHT-25);
+		outputPane.getChildren().add(consolePane);
+
+		
+		// Set the stage to the UI
+		Stage mainStage = primaryStage;
+	    mainStage.setScene(outputScene);
+	    mainStage.setResizable(false);
+	    m.setMainStage(mainStage);
+	    
+	    // Show the result!
+	    m.getMainStage().show();
+
+	}
+
+}
+
--- JavaBackend/src/MiddleEarth/Controller.java
+++ JavaBackend/src/MiddleEarth/Controller.java
@@ -3,40 +3,137 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import Backend.*;
-import FrontEnd.FrontEnd;
+import FrontEnd.View;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.GridPane;
+import model.Model;
 
-public class Controller {
+public class Controller{
 	
-	FileStorage storage;
-	FrontEnd view;
+	private FileStorage storage;
 	
-	//Constructor for the controller object
-	public Controller(FrontEnd view) {
-		this.view = view;
+	// Constructor for the controller object
+	public Controller() {
 		storage = new FileStorage();
 	}
 	
-	//Compiles and runs the list of files
-	public void compileAndRun(ArrayList<String> files) {
+	// Compiles and runs the list of files
+	public void compileAndRun(ArrayList<String> files, Model m) {
 		
-		//Gets the directory from the view if is does not already exist
+		// Gets the directory from the view if is does not already exist
 		if(!storage.hasDirectory()) {
-			File directory = view.getSelectedDirectory();
+			File directory = m.getSelectedDirectory();
 			storage.setDirectory(directory);
 		}
 		
-		//Setting the storage file
+		// Setting the storage file
 		storage.clear();
 		storage.addAll(files);
 		String outputs = "";
 		
-		//Compiles the code and returns the outputs
+		// Compiles the code and returns the outputs
 		try {outputs = GCompile.compileAndRun(storage);} 
 		catch (IOException error) {}
 		
-		//Passes the outputs to the view
-		view.setOutputText(outputs);
+		// Passes the outputs to the view
+		m.setOutputText(outputs);
 	}
 	
+	/**
+	 * These are listeners that allow the user to dynamically add more google docs
+	 * 
+	 */
+    public void pressButton(ActionEvent e, Model m){
+    	
+    	// Check if the action is a plus button
+    	if(e.getSource().toString().contains("+")){
+    		
+    		// Create a new button to add right under on the grid
+    		Button tempAddBtn = new Button("+");
+    		tempAddBtn.setTooltip(new Tooltip("Add more Google documents to compile"));
+    		
+    		// Add an action listener to the newly created button
+    		tempAddBtn.setOnAction(e2 -> pressDynamicButton (e2, m));
+    		
+    		// Increment the number of file fields needed to be checked
+    		m.setNumAddedFiles(m.getNumAddedFiles() +1);
+    		
+    		// Get the current grid pane to update
+    		GridPane grid = m.getGridpane();
+    			// add the new plus button
+			grid.add(tempAddBtn, 3, m.getNumPresetFilesToAdd() 
+					+ m.getNumAddedFiles());
+				// add new label for the input text field
+    		grid.add(new Label("Google Doc " + (m.getNumPresetFilesToAdd() 
+    										+ m.getNumAddedFiles()) + ":"),
+    				0, 
+    				m.getNumPresetFilesToAdd() + m.getNumAddedFiles());
+    			// add new text field for additional google doc
+    		grid.add(new TextField(), 1, m.getNumPresetFilesToAdd() + m.getNumAddedFiles());
+    		
+    		// Update the grid pane in model
+    		m.setGridpane(grid);
+    	}
+    	// Check if the action is a compile button
+    	else if(e.getSource().toString().contains("Compile")){
+    		
+    		// Get details from model
+    		GridPane grid = m.getGridpane();
+    		ArrayList<String> files = new ArrayList<String>();
+    		
+    		// Loop through text fields and get the text
+    		for(Node n: grid.getChildren()){
+    			if ( grid.getColumnIndex(n) == 1 && n instanceof TextField 
+    					&& ((TextField)n).getText().length() > 10){
+    				files.add( ((TextField)n).getText() );
+    			}
+    		}
+    		
+    		// Update model
+    		m.setFiles(files);
+    		
+    		// Compile the google doc files
+    		compileAndRun(m.getFiles(), m);
+    		
+    		// Show the output!
+    		m.getMainStage().show();
+    	}
+    
+    }
+    
+    /**
+     *  This second listener allows the user to stay on the same panel while seeing
+     *  updates to the UI 
+     */
+    public void pressDynamicButton(ActionEvent e2, Model m){
+    	
+    	// Check if the action is a plus button
+    	if(e2.getSource().toString().contains("+")){
+    		
+    		// Create a new button to add right under on the grid
+    		Button tempAddBtn = new Button("+");
+    		tempAddBtn.setTooltip(new Tooltip("Add more Google documents to compile"));
+    		tempAddBtn.setOnAction(e -> pressButton(e, m));
+    		
+    		// Increment the number of file fields needed to be checked
+    		m.setNumAddedFiles(m.getNumAddedFiles() +1);
+    		
+    		// Get the current grid pane to update with the new input labels
+    		GridPane grid = m.getGridpane();
+    		grid.add(tempAddBtn, 3, m.getNumPresetFilesToAdd() + m.getNumAddedFiles());
+    		grid.add(new Label("Google Doc " + (1 + m.getNumAddedFiles()) + ":"), 
+    				0, m.getNumPresetFilesToAdd()+ m.getNumAddedFiles());
+    		grid.add(new TextField(), 1, m.getNumPresetFilesToAdd() + m.getNumAddedFiles());
+    		
+    		// Update the grid pane in model
+    		m.setGridpane(grid);
+    	}
+    }
 
 }
